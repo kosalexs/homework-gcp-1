2574aeb6a0c084492f732a4a2b3569b1ef603494
Konstantin Alekseev <kosalexs@mail.ru>
2019-07-29 22:10:29 +0300

    change app module

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -65,3 +65,17 @@ resource "google_compute_firewall" "firewall_puma" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_proxy" {
+  name    = "allow-proxy-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+
+    ports = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
